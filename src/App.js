299097a7b26af6b3c93c9c915e0ac5bb3a729f38
Zakhar Ukhanov <ukhanovza@gmail.com>
2022-08-02 18:35:39 +0300

    Add styles from UI Kit witout attractive button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>My Happy Dress Demo</p>
-            </header>
+            <h1>Start</h1>
         </div>
     );
 }
