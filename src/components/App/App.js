56f899482e6d3345cb5f2418ee4582d64a8922d2
Zakhar Ukhanov <ukhanovza@gmail.com>
2022-08-04 01:14:51 +0300

    HAP-70: Add SCSS and Null styles + Fix file structure

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './App.module.scss';
+
+function App() {
+    return (
+        <div className={s.App}>
+            <h1>Start</h1>
+        </div>
+    );
+}
+
+export default App;
