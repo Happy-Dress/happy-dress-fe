ee66b2d02c58fa5d7025460b51b58c0b0d5d266e
LGers <87089817+LGers@users.noreply.github.com>
2022-09-04 12:26:30 +0300

    feat(HAP-55) add ButtonAccent functionality, styles

--- src/components/Buttons/ButtonAccent/ButtonAccent.jsx
+++ src/components/Buttons/ButtonAccent/ButtonAccent.jsx
@@ -3,25 +3,30 @@ import s from './ButtonAccent.module.scss';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-const ButtonAccent = (props) => {
+const ButtonAccent = ({ text, disabled, onClick }) => {
     const [active, setActive] = useState(false);
 
     const changeState = () => {
+        onClick();
         setActive(true);
-        setTimeout(() => setActive( false), 200);
+        setTimeout(() => setActive(false), 200);
     };
 
     return (
-        <button onClick={changeState}
+        <button
+            onClick={changeState}
             className={classNames(s.ButtonAccent, active ? s.ButtonAccent_active : '')}
+            disabled={disabled}
         >
-            {props.text}
+            {text}
         </button>
     );
 };
 
 ButtonAccent.propTypes = {
     text: PropTypes.string.isRequired,
+    onClick: PropTypes.func,
+    disabled: PropTypes.bool,
 };
 
 export default ButtonAccent;
