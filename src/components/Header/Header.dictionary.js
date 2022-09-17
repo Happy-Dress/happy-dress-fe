9a349fa89978087e14d8893f9b572b33a3cf7cb9
darktend <softwaredarktend@gmail.com>
2022-09-18 00:55:07 +0300

    HAP-37 fixed adaptive page

---
+++ src/components/Header/Header.dictionary.js
@@ -0,0 +1,7 @@
+
+const HEADER_DICTIONARY={
+    HEADER_TITLE:'HAPPYDRESS',
+    HEADER_NAV_ITEMS:['Главная','Каталог','Блог','Контакты','Примерка'],
+    HEAD_NUMBER:'+375 (29) 537 54 78'
+};
+export {HEADER_DICTIONARY};
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,24 +3,26 @@ import s from './Header.module.scss';
 import hanger from '../../images/hanger.svg';
 import burger from '../../images/burger.svg';
 import x from '../../images/x.svg';
+import { HEADER_DICTIONARY } from './Header.dictionary';
 
+const{
+    HEADER_TITLE,
+    HEADER_NAV_ITEMS,
+    HEAD_NUMBER
+}=HEADER_DICTIONARY;
 const Header = () => {
     const [activeMenu, setActiveMenu] = useState(false);
     return (
         <>
             <div className={s.Header}>
                 <div className={s.Header_left}>
-                    <p className={s.Header_logo}>HAPPYDRESS</p>
+                    <p className={s.Header_logo}>{HEADER_TITLE}</p>
                     <ul className={s.Header_menu}>
-                        <li className={s.Header_menu__item}>Главная</li>
-                        <li className={s.Header_menu__item}>Каталог</li>
-                        <li className={s.Header_menu__item}>Блог</li>
-                        <li className={s.Header_menu__item}>Контакты</li>
-                        <li className={s.Header_menu__item}>Примерка</li>
+                        {HEADER_NAV_ITEMS.map((item)=>(<li key={item} className={s.Header_menu__item}><span>{item}</span></li>))}
                     </ul>
                 </div>
                 <div className={s.Header_right}>
-                    <p className={s.Header_number}>+375 (29) 537 54 78</p>
+                    <p className={s.Header_number}>{HEAD_NUMBER}</p>
                     <div className={s.Header_cart}>
                         <img alt="hanger" src={hanger} />
                         <p>0</p>
@@ -51,13 +53,9 @@ const Header = () => {
             {activeMenu ? (
                 <div className={s.Header_mobile_modal}>
                     <ul className={s.Header_mobile_menu}>
-                        <li className={s.Header_mobile_menu__item}>Главная</li>
-                        <li className={s.Header_mobile_menu__item}>Каталог</li>
-                        <li className={s.Header_mobile_menu__item}>Блог</li>
-                        <li className={s.Header_mobile_menu__item}>Контакты</li>
-                        <li className={s.Header_mobile_menu__item}>Примерка</li>
-                        <li className={s.Header_mobile_menu__item}>
-                            +375 (29) 537 54 78
+                        {HEADER_NAV_ITEMS.map((item)=>(<li key={item} className={s.Header_menu__item}><span>{item}</span></li>))}
+                        <li className={s.Header_mobile_menu__number}>
+                            {HEAD_NUMBER}
                         </li>
                     </ul>
                 </div>
