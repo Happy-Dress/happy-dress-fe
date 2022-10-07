cf36832c8b4749d136de9cf03e5b898d3eefa7cf
darktend <softwaredarktend@gmail.com>
2022-10-07 16:05:14 +0300

    HAP-38:fix code

--- src/components/Footer/Footer.dictionary.js
+++ src/components/Footer/Footer.dictionary.js
@@ -1,7 +1,7 @@
 const FOOTER_DICTIONARY = {
     FOOTER_LOGO: 'HAPPYDRESS',
     FOOTER_NAV_ITEMS: ['Главная', 'Каталог', 'Блог', 'Контакты', 'Примерка'],
-    FOOTER_MOBILE_NAV_ITEMS: ['Главная', 'Каталог', 'Примерка', 'Контакты', 'Блог' ],
+    FOOTER_MOBILE_NAV_ITEMS: ['Главная', 'Каталог', 'Блог','Контакты','Примерка'],
     PHONE_NUMBER: '+375 (29) 590 80 29',
     FOOTER_EMAIL: 'happydress@gmail.com',
     FOOTER_SIGNATURE:['текст рыбка(год).что-то','г.Витебск, пр-т Фрунзе 81/33, ТЦ Эвиком,  2 эт. пав. 500'],
