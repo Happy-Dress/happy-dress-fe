3abc7fa0b96ff7cc2b315e812bb1dee17cbf4bda
Maksim Baravy <maksim.baravy@ukg.com>
2022-11-09 21:20:20 +0300

    bla vla vla

--- src/features/admin/Admin.jsx
+++ src/features/admin/Admin.jsx
@@ -7,6 +7,7 @@ const Admin = () => {
         <Routes>
             <Route path='/' element={<Navigate to='sign-in'/>}/>
             <Route path='/sign-in' element={<SignIn/>}/>
+
         </Routes>
     );
 };
