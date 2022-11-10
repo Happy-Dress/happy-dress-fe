bb104a893bbb96a6d179b477666623735668dd82
Koderkup <koderkup@gmail.com>
2022-11-10 17:42:30 +0300

    feat(HAP-92): admin panel

---
+++ src/features/admin/components/NavigationPanel.jsx
@@ -0,0 +1,69 @@
+/* eslint-disable indent */
+import React, { useState, useEffect } from 'react';
+import s from './NavigationPanel.module.scss';
+import './NavigationPanelStyle.css';
+import { Link, NavLink } from 'react-router-dom';
+import { ReactComponent as Exit } from '../../../assets/images/exit.svg';
+import { ReactComponent as Catalog } from '../../../assets/images/catalog.svg';
+import { ReactComponent as Goods } from '../../../assets/images/goods.svg';
+import { ReactComponent as Registration } from '../../../assets/images/registration.svg';
+import { ReactComponent as Blog } from '../../../assets/images/blog.svg';
+import HAPPYDRESS from '../../../assets/images/HAPPYDRESS.svg';
+
+function NavigationPanel() {
+const [quanty, setQuanty] = useState(0);
+const setActive = ({ isActive }) => (isActive ? 'active' : '');
+
+// function api() {
+//   return Promise.resolve(5);
+// }
+
+useEffect(() => {
+  // api().then((val) =>setQuanty(val));
+  fetch('https://jsonplaceholder.typicode.com/users/')
+    .then((response) => response.json())
+    .then((json) =>
+    json.forEach(element => 
+      setQuanty(prevQuanty=> prevQuanty+element.id)
+    ))   
+    .catch((err) => console.error('something wrong happened',err));
+}, []);
+
+  return (
+    <div className={s.navbar}>
+      <div className={s.navbarList}>
+        <div className={s.logo}>
+          <img src={HAPPYDRESS} alt="HAPPYDRESS" />
+        </div>
+
+        <NavLink to="/admin/panel/catalog-setting" className={setActive}>
+          <Catalog className='img'/>
+          <p>Каталог</p>
+        </NavLink>
+
+        <NavLink to="/admin/panel/goods-setting" className={setActive}>
+          <Goods className='img'/>
+          <p>Товар</p>
+        </NavLink>
+
+        <NavLink to="/admin/panel/registration-setting" className={setActive}>
+          <Registration className='img'/>
+          <p>Записи</p>
+          <p className={s.register}>{quanty}</p>
+        </NavLink>
+
+        <NavLink to="/admin/panel/blog-setting" className={setActive}>
+          <Blog className='img'/>
+          <p>Блог</p>
+        </NavLink>
+
+        <Link to="/domain/home">
+          <Exit />
+          <p>Выход</p>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default NavigationPanel;
---
+++ src/features/admin/pages/catalogsetting/CatalogSetting.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import s from './CatalogSetting.module.scss';
+const CatalogSetting = () => {
+    return (
+        <div className={s.div}>
+            <h1>CatalogSetting</h1>
+        </div>
+    );
+};
+
+export default CatalogSetting;
