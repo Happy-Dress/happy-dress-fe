d10dbd218cc66cdce3e460cf511cd202e15c37ac
eviv2206 <zhenya.saprin@mail.ru>
2022-11-13 22:48:46 +0300

    feat(HAP-51): add submit and validation in authorization form

--- src/features/admin/pages/SignIn/components/AuthorizationForm/AuthorizationForm.dictionary.js
+++ src/features/admin/pages/SignIn/components/AuthorizationForm/AuthorizationForm.dictionary.js
@@ -3,7 +3,14 @@ const AUTHORIZATION_FORM_DICTIONARY = {
     HELP_YOUR_NAME_LABEL: 'Ваше Имя',
     HELP_PASSWORD_LABEL: 'Пароль',
     BUTTON_ENTER_LABEL: 'Войти',
-    MAX_LENGTH_INPUT: 22,
+    MIN_CREDS_LENGTH: 4,
+    MAX_LOGIN_LENGTH: 35,
+    MAX_PASSWORD_LENGTH: 15,
+    TOO_SHORT_LOGIN_MESSAGE: 'Слишком короткое имя',
+    TOO_LONG_LOGIN_MESSAGE: 'Слишком длинное имя',
+    TOO_SHORT_PASSWORD_MESSAGE: 'Слишком короткий пароль',
+    TOO_LONG_PASSWORD_MESSAGE: 'Слишком длинный пароль',
+    POST_PATH: 'http://localhost:8080/api/v1/auth/login',
 };
 
 export { AUTHORIZATION_FORM_DICTIONARY };
