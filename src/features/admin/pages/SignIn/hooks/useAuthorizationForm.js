5d911b88df34d9107c77b26fc19f4072408a248f
eviv2206 <zhenya.saprin@mail.ru>
2022-11-20 01:38:30 +0300

    feat(HAP-51): update hook

--- src/features/admin/pages/SignIn/hooks/useAuthorizationForm.js
+++ src/features/admin/pages/SignIn/hooks/useAuthorizationForm.js
@@ -3,6 +3,7 @@ import { AUTHORIZATION_FORM_DICTIONARY } from '../components/AuthorizationForm/A
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useState } from 'react';
+import authenticateUser from '../../../api/authenticateUser';
 
 const {
     MIN_CREDS_LENGTH,
@@ -14,31 +15,44 @@ const {
     TOO_LONG_PASSWORD_MESSAGE,
 } = AUTHORIZATION_FORM_DICTIONARY;
 
-
 const authorizationCredentials = z.object({
     login: z.string().min(MIN_CREDS_LENGTH, TOO_SHORT_LOGIN_MESSAGE).max(MAX_LOGIN_LENGTH, TOO_LONG_LOGIN_MESSAGE),
     password: z.string().min(MIN_CREDS_LENGTH, TOO_SHORT_PASSWORD_MESSAGE).max(MAX_PASSWORD_LENGTH, TOO_LONG_PASSWORD_MESSAGE),
 });
 
-const useAuthorizationForm = (initialVisibilityPic, initialInputState) => {
-    const [toggleIcon, setToggleIcon] = useState(initialVisibilityPic);
-    const [type, setType] = useState(initialInputState);
+
+const useAuthorizationForm = () => {
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
 
     const { register, setError, formState: { errors, isValid }, handleSubmit } = useForm({
         mode: 'onBlur',
-        resolver: zodResolver(authorizationCredentials)
+        resolver: zodResolver(authorizationCredentials),
     });
 
+    const onAuthenticateUser = async (credentials) => {
+        try {
+            const token = await authenticateUser(credentials);
+            localStorage.setItem('Authorization', `Bearer ${token}`);
+        } catch (e) {
+            // TODO handle error with alert notifications
+            console.log(e);
+        }
+    };
+
+    const onSubmit = handleSubmit(onAuthenticateUser);
+
+    const togglePasswordVisibility = () => {
+        setIsPasswordVisible(!isPasswordVisible);
+    };
+
     return {
+        onSubmit,
         register,
         setError,
         errors,
         isValid,
-        handleSubmit,
-        toggleIcon,
-        setToggleIcon,
-        type,
-        setType,
+        isPasswordVisible,
+        togglePasswordVisibility
     };
 };
 
