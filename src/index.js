a1632b339f0bc4b36aee2f772e259e0dba1e2319
eviv2206 <zhenya.saprin@mail.ru>
2022-11-20 01:34:51 +0300

    feat(HAP-51): add axios default config

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.module.scss';
 import App from './common/components/App/App';
 import { BrowserRouter } from 'react-router-dom';
+import AxiosConfigProvider from './axiosConfig';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <BrowserRouter>
-            <App />
+            <AxiosConfigProvider>
+                <App/>
+            </AxiosConfigProvider>
         </BrowserRouter>
     </React.StrictMode>
 );
