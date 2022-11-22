67c39dcd861d9051da168638326a76d7b1bfaa33
Zakhar Ukhanov <ukhanovza@gmail.com>
2022-11-22 20:19:50 +0300

    HAP-86: Add basic structure

--- src/features/admin/Admin.jsx
+++ src/features/admin/Admin.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
+import CatalogSettings from './pages/CatalogSettings/CatalogSettings';
 import SignIn from './pages/SignIn';
 
 const Admin = () => {
@@ -7,6 +8,7 @@ const Admin = () => {
         <Routes>
             <Route path='/' element={<Navigate to='sign-in'/>}/>
             <Route path='/sign-in' element={<SignIn/>}/>
+            <Route path='/catalog-settings' element={<CatalogSettings/>}/>
         </Routes>
     );
 };
---
+++ src/features/admin/pages/CatalogSettings/CatalogSettings.jsx
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+import s from './CatalogSettings.module.scss';
+import down from '../../../../assets/images/down.svg';
+import ButtonDefault from '../../../../common/components/Buttons/ButtonDefault';
+import ButtonAccent from '../../../../common/components/Buttons/ButtonAccent';
+
+
+const CatalogSettings = () => {
+
+    
+    const [items, setItems] = useState([
+        {
+            name: 'Категории',
+            active: false,
+            state: []
+        },
+        {
+            name: 'Цвет',
+            active: false,
+            state: []
+        },
+        {
+            name: 'Материал',
+            active: false,
+            state: []
+        },
+        {
+            name: 'Модель',
+            active: false,
+            state: []
+        },
+    ]);
+    console.log(items);
+
+
+    return(
+        <div className={s.CatalogSettings}>
+            <div className={s.Way}>Главная &gt; Управление каталогом</div>
+            <h2>Управление каталогом</h2>
+            <div className={s.categories}>
+                {
+                    items.map((el, index) => {return(
+                        <div className={s.category} key={index} onClick={() => setItems(items.map((e, i) => index === i ? { ...e, active: !el.active }: e))}>
+                            <h3>{el.name}</h3>
+                            <img src={down} alt='arrowDown'
+                                className={el.active ? s.active : s.img}/>
+                        </div>
+                    );})
+                }    
+            </div>
+            <div className={s.buttons}>
+                <ButtonDefault text='Отмена'/>
+                <ButtonAccent text='Сохранить'/>
+            </div>
+        </div>
+    );
+};
+
+export default CatalogSettings;
+
+
+// () => setItems(items.map((e, i) => index === i ? { ...e, active: !el.active }: e))
