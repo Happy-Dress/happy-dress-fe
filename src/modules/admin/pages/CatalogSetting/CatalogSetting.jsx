d86113322949a3b3168c21ac9d7063a2506aa21c
Zakhar Ukhanov <ukhanovza@gmail.com>
2022-11-30 17:35:41 +0300

    HAP-86: Fix styles

--- src/modules/admin/pages/CatalogSetting/CatalogSetting.jsx
+++ src/modules/admin/pages/CatalogSetting/CatalogSetting.jsx
@@ -41,8 +41,7 @@ const CatalogSettings = () => {
                         <div  className={s.category} key={index} >
                             <div className={s.title} onClick={() => setItems(items.map((e, i) => index === i ? { ...e, active: !el.active }: e))}>
                                 <h3>{el.name}</h3>
-                                <img src={down} alt='arrowDown'
-                                    className={el.active ? s.active : s.img}/>
+                                <svg className={el.active ? s.active : s.img} xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 256 256"><path fill="#212121" d="M128 184a8.5 8.5 0 0 1-5.7-2.3l-80-80a8.1 8.1 0 0 1 11.4-11.4l74.3 74.4l74.3-74.4a8.1 8.1 0 0 1 11.4 11.4l-80 80a8.5 8.5 0 0 1-5.7 2.3Z"/></svg>                        
                             </div>
                             <div className={el.active ? s.items_active : s.items }>
                             </div>
