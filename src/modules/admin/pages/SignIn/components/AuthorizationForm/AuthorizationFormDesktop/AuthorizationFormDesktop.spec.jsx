74df870abda53cead4f6e8a04eee4859284cc8eb
eviv2206 <zhenya.saprin@mail.ru>
2022-12-11 00:32:35 +0300

    feat(HAP-51): add new test for Authorization Form Desktop

--- src/modules/admin/pages/SignIn/components/AuthorizationForm/AuthorizationFormDesktop/AuthorizationFormDesktop.spec.jsx
+++ src/modules/admin/pages/SignIn/components/AuthorizationForm/AuthorizationFormDesktop/AuthorizationFormDesktop.spec.jsx
@@ -1,42 +1,73 @@
 import React from 'react';
-import { screen, render, waitFor, fireEvent } from '@testing-library/react';
+import { screen, render, act, fireEvent } from '@testing-library/react';
 import AuthorizationFormDesktop from './AuthorizationFormDesktop';
 import userEvent from '@testing-library/user-event';
+import useAuthorizationForm from '../../../hooks/useAuthorizationForm';
 
-const props = {
-    onSubmit: jest.fn(),
-    errors: {},
-    register: jest.fn(),
-    isValid: false,
-    isPasswordVisible: false,
-    togglePasswordVisibility: jest.fn(),
+const Wrapper = () => {
+
+    const authorizationForm = useAuthorizationForm();
+
+    return (
+        <>
+            <AuthorizationFormDesktop {...authorizationForm}/>
+        </>
+    );
 };
 
+const mockedUseNavigate = jest.fn();
+jest.mock('react-router-dom', () => ({
+    useNavigate: () => mockedUseNavigate,
+}));
+
+jest.mock('../../../../../api/authenticateUser', () => (
+    jest.fn()
+));
+
 describe('AuthorizationFormDesktop', () => {
+
+    beforeEach(() => {
+        render(<Wrapper/>);
+    });
+
     it('should render authorization form desktop', async () => {
-        const { baseElement } = render(<AuthorizationFormDesktop {...props}/>);
-        expect(baseElement).toBeInTheDocument();
+        const panelDesktop = screen.getByText('ВХОД');
+        expect(panelDesktop).toBeInTheDocument();
     });
 
     it('should press enter button', async () => {
-        render(<AuthorizationFormDesktop {...props}/>);
-        const BtnEnter = screen.getByText('Войти');
-        BtnEnter.disabled = false;
-        await waitFor(() => {
-            userEvent.click(BtnEnter);
+        await act(() => {
+            userEvent.paste(screen.getByPlaceholderText('Ваше Имя'), 'admin');
         });
-        expect(props.onSubmit).toHaveBeenCalledTimes(1);
+        await act(() => {
+            userEvent.paste(screen.getByPlaceholderText('Пароль'), 'admin');
+        });
+        await act(() => {
+            userEvent.click(screen.getByText('Войти'));
+        });
+        expect(mockedUseNavigate).toHaveBeenCalledTimes(1);
     });
 
     it('should change password visibility', async () => {
-        render(<AuthorizationFormDesktop {...props}/>);
         const PicVisibility = screen.getByAltText('visibility icon');
-        userEvent.click(PicVisibility);
-        expect(props.togglePasswordVisibility).toHaveBeenCalledTimes(1);
+        await act(() => {
+            userEvent.click(PicVisibility);
+        });
+        const PasswordInput = screen.getByPlaceholderText('Пароль');
+        expect(PasswordInput.type).toBe('text');
     });
 
     it('should render error login message', async () => {
-        fireEvent.blur(screen.getByPlaceholderText('Ваше имя'));
+        await act(() => {
+            fireEvent.blur(screen.getByPlaceholderText('Ваше Имя'));
+        });
         expect(screen.getByText('Слишком короткое имя')).toBeInTheDocument();
+    });
+
+    it('should render error password message', async () => {
+        await act(() => {
+            fireEvent.blur(screen.getByPlaceholderText('Пароль'));
+        });
+        expect(screen.getByText('Слишком короткий пароль')).toBeInTheDocument();
     });
 });
