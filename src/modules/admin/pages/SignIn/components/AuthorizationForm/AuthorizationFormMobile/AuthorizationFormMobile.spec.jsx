fec7bbf1f7bc93b82bdd5d89b83db6c86277d095
eviv2206 <zhenya.saprin@mail.ru>
2022-12-11 00:33:05 +0300

    feat(HAP-51): add new test for Authorization Form mobile

--- src/modules/admin/pages/SignIn/components/AuthorizationForm/AuthorizationFormMobile/AuthorizationFormMobile.spec.jsx
+++ src/modules/admin/pages/SignIn/components/AuthorizationForm/AuthorizationFormMobile/AuthorizationFormMobile.spec.jsx
@@ -1,27 +1,76 @@
 import React from 'react';
-import { screen, render, waitFor } from '@testing-library/react';
-import AuthorizationFormDesktop from './AuthorizationFormMobile';
+import { screen, render, waitFor, act, fireEvent } from '@testing-library/react';
+import useAuthorizationForm from '../../../hooks/useAuthorizationForm';
+import AuthorizationFormMobile from './AuthorizationFormMobile';
+import userEvent from '@testing-library/user-event';
 
-jest.mock('./AuthorizationFormMobile', () => ({
-    __esModule: true,
-    default: () => <div>Authorization form mobile</div>
-}));
+const Wrapper = () => {
 
-const props = {
-    onSubmit: jest.fn(),
-    errors: {},
-    register: jest.fn(),
-    isValid: false,
-    isPasswordVisible: false,
-    togglePasswordVisibility: jest.fn(),
+    const authorizationForm = useAuthorizationForm();
+
+    return (
+        <>
+            <AuthorizationFormMobile {...authorizationForm}/>
+        </>
+    );
 };
 
+const mockedUseNavigate = jest.fn();
+jest.mock('react-router-dom', () => ({
+    useNavigate: () => mockedUseNavigate,
+}));
+
+jest.mock('../../../../../api/authenticateUser', () => (
+    jest.fn()
+));
+
+
 describe('AuthorizationFormMobile', () => {
+
+    beforeEach(() => {
+        render(<Wrapper/>);
+    });
+
     it('should render authorization form mobile', async () => {
-        render(<AuthorizationFormDesktop { ...props }/>);
-        const desktopPanel = screen.getByText('Authorization form mobile');
+        const mobilePanel = screen.getByText('ВХОД');
         await waitFor(() => {
-            expect(desktopPanel).toBeInTheDocument();
+            expect(mobilePanel).toBeInTheDocument();
         });
+    });
+
+    it('should press enter button', async () => {
+        await act(() => {
+            userEvent.paste(screen.getByPlaceholderText('Ваше Имя'), 'admin');
+        });
+        await act(() => {
+            userEvent.paste(screen.getByPlaceholderText('Пароль'), 'admin');
+        });
+        await act(() => {
+            userEvent.click(screen.getByText('Войти'));
+        });
+        expect(mockedUseNavigate).toHaveBeenCalledTimes(1);
+    });
+
+    it('should change password visibility', async () => {
+        const PicVisibility = screen.getByAltText('visibility icon');
+        await act(() => {
+            userEvent.click(PicVisibility);
+        });
+        const PasswordInput = screen.getByPlaceholderText('Пароль');
+        expect(PasswordInput.type).toBe('text');
+    });
+
+    it('should render error login message', async () => {
+        await act(() => {
+            fireEvent.blur(screen.getByPlaceholderText('Ваше Имя'));
+        });
+        expect(screen.getByText('Слишком короткое имя')).toBeInTheDocument();
+    });
+
+    it('should render error password message', async () => {
+        await act(() => {
+            fireEvent.blur(screen.getByPlaceholderText('Пароль'));
+        });
+        expect(screen.getByText('Слишком короткий пароль')).toBeInTheDocument();
     });
 });
