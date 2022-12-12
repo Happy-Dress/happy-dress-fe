63835b1a9e8fff4a4cef60814ef040aa90c894af
Zakhar Ukhanov <ukhanovza@gmail.com>
2022-12-12 15:01:44 +0300

    HAP-86: Fix svg

--- src/modules/admin/pages/CatalogSetting/CatalogSetting.jsx
+++ src/modules/admin/pages/CatalogSetting/CatalogSetting.jsx
@@ -36,16 +36,16 @@ const CatalogSettings = () => {
             <h2>Управление каталогом</h2>
             <div className={s.categories}>
                 {
-                    items.map((el, index) => {return(
+                    items.map((el, index) => (
                         <div  className={s.category} key={index} >
                             <div className={s.title} onClick={() => setItems(items.map((e, i) => index === i ? { ...e, active: !el.active }: e))}>
                                 <h3>{el.name}</h3>
-                                <svg className={el.active ? s.active : s.img} xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 256 256"><path fill="#212121" d="M128 184a8.5 8.5 0 0 1-5.7-2.3l-80-80a8.1 8.1 0 0 1 11.4-11.4l74.3 74.4l74.3-74.4a8.1 8.1 0 0 1 11.4 11.4l-80 80a8.5 8.5 0 0 1-5.7 2.3Z"/></svg>                        
+                                <svg className={el.active ? s.active : s.img} xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 256 256"><path/></svg>                        
                             </div>
                             <div className={el.active ? s.items_active : s.items }>
                             </div>
                         </div>
-                    );})
+                    ))
                 }    
             </div>
             <div className={s.buttons}>
