eced04d8d05a2c06d982123e578a48dbca833c76
darktend <softwaredarktend@gmail.com>
2022-12-14 09:14:36 +0200

    HAP-43:fix node

--- src/modules/domain/pages/Landing/Categories/Categories.jsx
+++ src/modules/domain/pages/Landing/Categories/Categories.jsx
@@ -8,7 +8,7 @@ const Categories=()=>{
     return(
         <>
             {isDesktop && <CategoriesDesktop />}
-            {isMobile && <CategoryMobileCard />}
+            {isMobile && <CategoriesMobileCard />}
         </>
     );
 };
