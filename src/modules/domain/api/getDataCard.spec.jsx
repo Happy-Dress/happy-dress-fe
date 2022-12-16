d2c7d4e536c1e04d0d8a906ef89ecfba68d98472
darktend <softwaredarktend@gmail.com>
2022-12-16 08:11:26 +0200

    HAP-43:add test api

--- src/modules/domain/api/getDataCard.spec.jsx
+++ src/modules/domain/api/getDataCard.spec.jsx
@@ -1,20 +1,9 @@
-import getDataCards from './getDataCards';
+import getDataCards  from './getDataCards';
 
-function fetchDataPromiseWithErrorMessage() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            reject('error');
-        }, 100);
-    });
-}
 describe('getDataCards',  () => {
-    it('the fetch fails with an error', async () => {
-        expect.assertions(1);
-        try {
-            await fetchDataPromiseWithErrorMessage();
-        } catch (e) {
-            expect(e).toMatch('error');
-        }
+    it('should return amount of orders',  () => {
+        getDataCards().then(async (data)=>
+            expect(data).toBe(10));
     });
 });
 
