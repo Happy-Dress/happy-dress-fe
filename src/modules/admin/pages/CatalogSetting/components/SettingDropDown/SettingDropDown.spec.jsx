039450eb746de13b214e14e2bf8b2262116cf0fa
eviv2206 <zhenya.saprin@mail.ru>
2023-01-21 20:53:25 +0300

    feat(HAP-86): update test

--- src/modules/admin/pages/CatalogSetting/components/SettingDropDown/SettingDropDown.spec.jsx
+++ src/modules/admin/pages/CatalogSetting/components/SettingDropDown/SettingDropDown.spec.jsx
@@ -1,7 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 
 import React from 'react';
 import SettingsDropDown from './SettingDropDown';
+import userEvent from '@testing-library/user-event';
 
 describe('SettingDropDown', () => {
     it('should render correctly', async () => {
@@ -10,5 +11,18 @@ describe('SettingDropDown', () => {
         render(<SettingsDropDown name={name}>{children}</SettingsDropDown>);
         const page = screen.getByText('Sample');
         expect(page).toBeInTheDocument();
+    });
+
+    it('should press elem correctly', async () => {
+        const title = 'Sample';
+        const mockText = 'Text';
+        const child = <p>{mockText}</p>;
+        render(<SettingsDropDown name={title}>{child}</SettingsDropDown>);
+        const titleElem = screen.getByText('Sample');
+        userEvent.click(titleElem);
+        const childElem = screen.getByText(mockText);
+        waitFor(() => {
+            expect(childElem).toBeInTheDocument(); 
+        });
     });
 });
