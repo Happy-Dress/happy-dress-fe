c0560e5d5c7e4424ef32c9aa5d26ba55ba8f1d64
eviv2206 <zhenya.saprin@mail.ru>
2023-01-25 02:32:19 +0300

    feat(HAP-170): add handling of 401 response

--- src/common/config/axiosConfig.jsx
+++ src/common/config/axiosConfig.jsx
@@ -4,11 +4,15 @@ axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
 
 axios.interceptors.response.use(
     (response) => response,
-    (error) =>
+    (error) => {
+        if (error.response.status === 401){
+            localStorage.Authorization.clear();
+        }
         Promise.reject(Array.isArray(error.response?.data?.message) ?
             error.response.data.message[0]
-            : (error.response.data.message || error.message)),
-);
+            : (error.response.data.message || error.message));
+    })
+;
 
 const AxiosConfigProvider = ({ children }) => {
     return children;
