e7bbb2a82a139fe2e146b0a6f7a0d3ddb379813a
eviv2206 <zhenya.saprin@mail.ru>
2023-01-25 02:36:55 +0300

    fix(HAP-160): fix bug of rendering footer and header on 1023px width

--- src/common/ui/contexts/DeviceType/provider/DeviceTypeProvider.js
+++ src/common/ui/contexts/DeviceType/provider/DeviceTypeProvider.js
@@ -5,9 +5,8 @@ import { useMediaQuery } from 'react-responsive';
 const DeviceTypeProvider = (props) => {
 
     const DESKTOP_MIN_SCREEN_SIZE = '1024px';
-    const MAX_MOBILE_SIZE='1023px';
     const isDesktop = useMediaQuery({ query: `(min-width: ${DESKTOP_MIN_SCREEN_SIZE})` });
-    const isMobile = useMediaQuery({ query: `(max-width: ${MAX_MOBILE_SIZE})` });
+    const isMobile = !isDesktop;
 
     return (
         <DeviceTypeContext.Provider value={{ isMobile, isDesktop }}>
