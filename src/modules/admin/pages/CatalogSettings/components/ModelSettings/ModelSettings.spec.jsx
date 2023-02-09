0af021e130f296067e31c8fc38e32fa4cd48f041
Maksim <Maksim_Baravy@epam.com>
2023-02-10 00:31:26 +0300

    chore(HAP-130): fix mobile style

--- src/modules/admin/pages/CatalogSettings/components/ModelSettings/ModelSettings.spec.jsx
+++ src/modules/admin/pages/CatalogSettings/components/ModelSettings/ModelSettings.spec.jsx
@@ -1,210 +1,28 @@
-import { render, screen, waitFor } from '@testing-library/react';
-import ModelSettings from './index';
-import userEvent from '@testing-library/user-event';
+import React from 'react';
+import { render } from '@testing-library/react';
+import ModelSettings from './ModelSettings';
 
-const mockUpdateModels = jest.fn();
-
-const mockModel = {
-    id: 1,
-    name: 'Прямое',
-    orderNumber: 0
-};
-
-jest.mock('../SettingsList', () => ({
+jest.mock('../SimpleSettingsControl/SimpleSettingsControl', () =>({
     __esModule: true,
-    default: ({ onEdit, onSelect, onUnSelect, handleReorder, onRemove }) => {
-        return (
-            <>
-                <button onClick={() => onEdit(mockModel)}>Edit</button>
-                <button onClick={() => onSelect(mockModel)}>Select</button>
-                <button onClick={() => onUnSelect(mockModel)}>Unselect</button>
-                <button onClick={handleReorder}>Reorder</button>
-                <button onClick={() => onRemove(mockModel)}>Remove</button>
-            </>
-        );
+    default: () => {
+        return <div>Test</div>;
     }
 }));
 
 jest.mock('../../contexts/CatalogSettingsContext/hook/useCatalogSettings', () => ({
     useCatalogSettings: () =>({
         settings: {
-            models: []
+            models: [],
+            materials: []
         },
-        updateModels: mockUpdateModels
+        updateMaterials: jest.fn()
     })
 }));
 
 
-describe('ModelSettings', () =>{
-    it('should render correctly', () =>{
+describe('ModelSettings', () => {
+    it('should render correctly' , () =>{
         const { baseElement } = render(<ModelSettings/>);
         expect(baseElement).toBeInTheDocument();
     });
-
-    it('should handle reorder', async () =>{
-        render(<ModelSettings/>);
-        const button = screen.getByRole('button', {
-            name: /Reorder/i
-        });
-        await waitFor(() =>{
-            userEvent.click(button);
-        });
-        expect(mockUpdateModels).toHaveBeenCalled();
-    });
-
-    it('should start edit and cancel', async () =>{
-        render(<ModelSettings/>);
-        const button = screen.getByRole('button', {
-            name: /Edit/i
-        });
-        await waitFor(() =>{
-            userEvent.click(button);
-        });
-        const cancelButton = screen.getByRole('button', {
-            name: /Отмена/i
-        });
-        await waitFor(() =>{
-            userEvent.click(cancelButton);
-        });
-        expect(cancelButton).not.toBeInTheDocument();
-    });
-
-
-
-    it('should start edit and save', async () =>{
-        render(<ModelSettings/>);
-        const button = screen.getByRole('button', {
-            name: /Edit/i
-        });
-        await waitFor(() =>{
-            userEvent.click(button);
-        });
-        const saveButton = screen.getByRole('button', {
-            name: /Сохранить/i
-        });
-        await waitFor(() =>{
-            userEvent.click(saveButton);
-        });
-        expect(mockUpdateModels).toHaveBeenCalled();
-    });
-
-    it('should handle add', async () =>{
-        render(<ModelSettings/>);
-        const button = screen.getByRole('button', {
-            name: '+Добавить'
-        });
-        await waitFor(() =>{
-            userEvent.click(button);
-        });
-        const cancelButton = screen.getByRole('button', {
-            name: /Отмена/i
-        });
-        expect(cancelButton).toBeInTheDocument();
-    });
-
-    it('should handle select models and delete them', async () =>{
-        render(<ModelSettings/>);
-        const button = screen.getByRole('button', {
-            name: 'Select'
-        });
-        await waitFor(() =>{
-            userEvent.click(button);
-        });
-        const deleteButton = screen.getByRole('button', {
-            name: /Удалить/i
-        });
-        await waitFor(() =>{
-            userEvent.click(deleteButton);
-        });
-        expect(deleteButton).not.toBeInTheDocument();
-    });
-    it('should handle remove', async () =>{
-        render(<ModelSettings/>);
-        const button = screen.getByRole('button', {
-            name: 'Remove'
-        });
-        await waitFor(() =>{
-            userEvent.click(button);
-        });
-        expect(mockUpdateModels).toHaveBeenCalled();
-    });
-
-    it('should handle unselect', async () =>{
-        render(<ModelSettings/>);
-        const button = screen.getByRole('button', {
-            name: 'Select'
-        });
-        await waitFor(() =>{
-            userEvent.click(button);
-        });
-        const unselectButton = screen.getByRole('button', {
-            name: 'Unselect'
-        });
-        const deleteButton = screen.getByRole('button', {
-            name: /Удалить/i
-        });
-        await waitFor(() =>{
-            userEvent.click(unselectButton);
-        });
-
-        expect(deleteButton).not.toBeInTheDocument();
-    });
-
-    it('should handle input change', async () =>{
-        render(<ModelSettings/>);
-        const button = screen.getByRole('button', {
-            name: /Edit/i
-        });
-        await waitFor(() =>{
-            userEvent.click(button);
-        });
-        const input = screen.getByDisplayValue('Прямое');
-        await waitFor((() =>{
-            userEvent.paste(input, 'Пышное');
-        }));
-        const cancelButton = screen.getByRole('button', {
-            name: /Отмена/i
-        });
-        expect(cancelButton).toBeInTheDocument();
-    });
-
-    it('should prevent remove', async () =>{
-        render(<ModelSettings/>);
-        const button = screen.getByRole('button', {
-            name: /Edit/i
-        });
-        await waitFor(() =>{
-            userEvent.click(button);
-        });
-        const removeButton = screen.getByRole('button', {
-            name: 'Remove'
-        });
-        await waitFor(() =>{
-            userEvent.click(removeButton);
-        });
-        expect(mockUpdateModels).not.toHaveBeenCalled();
-    });
-
-    it('should add model and save', async () =>{
-        render(<ModelSettings/>);
-        const button = screen.getByRole('button', {
-            name: '+Добавить'
-        });
-        await waitFor(() =>{
-            userEvent.click(button);
-        });
-        const input = screen.getByRole('textbox');
-        await waitFor(() =>{
-            userEvent.paste(input, 'Пышное');
-        });
-        const saveButton = screen.getByRole('button', {
-            name: /Сохранить/i
-        });
-        await waitFor(() =>{
-            userEvent.click(saveButton);
-        });
-        expect(mockUpdateModels).toHaveBeenCalled();
-
-    });
-
 });
