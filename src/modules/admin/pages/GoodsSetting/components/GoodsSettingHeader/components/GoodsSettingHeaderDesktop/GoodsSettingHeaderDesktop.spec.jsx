e2f3781966091039dcaeb1eed87e647127d23211
MaGTM <uporoff.maxim@yandex.ru>
2023-02-16 20:56:14 +0600

    feat(HAP-97): add tests for Goods Setting

--- src/modules/admin/pages/GoodsSetting/components/GoodsSettingHeader/components/GoodsSettingHeaderDesktop/GoodsSettingHeaderDesktop.spec.jsx
+++ src/modules/admin/pages/GoodsSetting/components/GoodsSettingHeader/components/GoodsSettingHeaderDesktop/GoodsSettingHeaderDesktop.spec.jsx
@@ -1,7 +1,8 @@
-import { render } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
 import GoodsSettingHeaderDesktop from './GoodsSettingHeaderDesktop';
 import { mockGoodsSettingContext } from '../../../../../../../../__mocks__/mockGoodsSettingContext';
 import { BrowserRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 
 jest.mock('../../../../contexts/CatalogProvider/useCatalogContext', () => ({
     useCatalogContext: () => ({ ...mockGoodsSettingContext })
@@ -33,5 +34,19 @@ describe('GoodsSettingHeaderDesktop', () => {
         const { container } = render(<GoodsSettingHeaderDesktop />, { wrapper: BrowserRouter });
 
         expect(container.getElementsByClassName('FilterBadge').length).toBe(0);
+    });
+
+    it('should open filters', async () => {
+        const { container } = render(<GoodsSettingHeaderDesktop/>, { wrapper: BrowserRouter });
+
+        expect(screen.getByText('filter.svg')).toBeInTheDocument();
+        expect(container.getElementsByClassName('Filters')[0]).toBe(undefined);
+
+        await act(() => {
+            userEvent.click(screen.getByText('filter.svg'));
+        });
+
+        expect(screen.getByText('x.svg')).toBeInTheDocument();
+        expect(container.getElementsByClassName('Filters')[0]).toBeInTheDocument();
     });
 });
