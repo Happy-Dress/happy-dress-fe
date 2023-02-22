f3639379287f8b1bf094f50ddb05041bcf1f3aa3
Maksim Baravy <maksim.baravy@ukg.com>
2023-02-22 13:33:54 +0300

    chore(HAP-185): add tests

---
+++ src/common/ui/components/Modal/ModalMobile/ModalMobile.spec.jsx
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react';
+import ModalMobile from './ModalMobile';
+
+describe('Modal Mobile', () =>{
+    it('should render correctly', async () =>{
+        const { baseElement } = render(<ModalMobile size='lg'/>);
+        expect(baseElement).toBeInTheDocument();
+    });
+});
