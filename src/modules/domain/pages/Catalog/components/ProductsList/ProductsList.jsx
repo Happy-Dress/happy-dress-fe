8bc4fbe9c83574e227773c358d3864db22ad7c2b
MaGTM <uporoff.maxim@yandex.ru>
2023-03-06 17:24:35 +0600

    fix(HAP-97): del product add

--- src/modules/domain/pages/Catalog/components/ProductsList/ProductsList.jsx
+++ src/modules/domain/pages/Catalog/components/ProductsList/ProductsList.jsx
@@ -1,5 +1,4 @@
 import s from './ProductsList.module.scss';
-import ProductCardAdd from '../ProductCardAdd';
 import { useSelector } from 'react-redux';
 import ProductCardSkeleton from '../../../../../../common/ui/components/ProductCard/ProductCardSkeleton';
 import ProductCard from '../../../../../../common/ui/components/ProductCard/ProductCard';
@@ -15,7 +14,6 @@ const ProductsList = () =>{
 
     return (
         <div className={s.ProductsList}>
-            <ProductCardAdd/>
             {products.map((product, index) => {
                 return <ProductCard key={index} product={product}/>;
             })}
