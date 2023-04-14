7e63a7bd8946d4353df5f2dd807e8acd1f6a8b1a
eviv2206 <zhenya.saprin@mail.ru>
2023-04-14 11:52:38 +0300

    feat(HAP-201): add category slider for mobile

--- src/modules/domain/pages/Landing/Categories/CategoriesMobile/CategoriesMobile.jsx
+++ src/modules/domain/pages/Landing/Categories/CategoriesMobile/CategoriesMobile.jsx
@@ -2,15 +2,36 @@ import React, { useState } from 'react';
 import s from './CategoriesMobile.module.scss';
 import PropTypes from 'prop-types';
 import CATEGORIES_DICTIONARY from '../Categories.dictionary';
+import { useSwipeable } from 'react-swipeable';
 import classNames from 'classnames';
-import Photo from '../../../../../../common/assets/images/photo_4_3.png';
 
 
 const { HEADING_LABEL } = CATEGORIES_DICTIONARY;
 
 const CategoriesMobile = ({ categories }) => {
 
-    const [sliderState] = useState(0);
+    const [index, setIndex] = useState(0);
+    const handleSwipe = useSwipeable({
+        onSwipedLeft: () => {
+            setIndex((prevIndex) => (prevIndex + 1) % categories.length);
+        },
+        onSwipedRight: () => setIndex((prevIndex) => (prevIndex + categories.length - 1) % categories.length),
+    });
+
+
+    const renderDots = () => {
+        return (
+            <div className={s.slider_dots}>
+                {categories.map((_, i) => (
+                    <span
+                        key={i}
+                        onClick={() => setIndex(i)}
+                        className={classNames(s.slider_dot, index === i ? s.slider_dot_active : s.slider_dot_disabled)}
+                    />
+                ))}
+            </div>
+        );
+    };
 
     return (
         <div className={s.CategoriesMobile}>
@@ -18,34 +39,28 @@ const CategoriesMobile = ({ categories }) => {
                 <h2>{HEADING_LABEL}</h2>
             </div>
             <div className={s.slider}>
-                <div className={s.bullets}>
-                    {
-                        categories?.map((post, index) => {
-                            return <span key={index} className={s.bullet}/>;
-                        })
-                    }
-                </div>
-                <div className={s.sliderWrapper}>
-                    {categories?.map((post, index) => (
-                        <div key={post.id} className={s.card}>
-                            <img
-                                src={Photo}
-                                alt={'category'}
-                                className={classNames({
-                                    [s.main]: index === sliderState,
-                                    [s.medium]: (index % 2 === 0),
-                                    [s.small]: (index % 2 === 1)
-                                })}
-                            />
-                            <h3>{post.name}</h3>
-                            <span>{post.description}</span>
+                {renderDots()}
+                <div {...handleSwipe}>
+                    {categories.map((post, key) => (
+                        <div className={classNames(key === index ? s.slider_card_active : s.slider_card)} key={key}>
+                            {key === index && <div>
+                                <img src={post.imageUrl} className={s.slider_card_mainImage}
+                                    alt={`Slide ${index + 1}`}/>
+                                <h3>{post.name}</h3>
+                                <div className={s.slider_card_description}>
+                                    <p>{post.description}</p>
+                                </div>
+                            </div>
+                            }
                         </div>
                     ))}
                 </div>
             </div>
         </div>
-    );
-};
+    )
+    ;
+}
+;
 CategoriesMobile.propTypes = {
     categories: PropTypes.arrayOf(PropTypes.shape({
         name: PropTypes.string.isRequired,
