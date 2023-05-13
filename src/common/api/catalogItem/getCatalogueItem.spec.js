6f440f7bcb15284f7ad5a7414a22acfeddee22a3
LGers <leonid.mychko2k21@gmail.com>
2023-05-13 20:08:24 +0300

    fix(HAP-199): Fix multispaces eslint error

--- src/common/api/catalogItem/getCatalogueItem.spec.js
+++ src/common/api/catalogItem/getCatalogueItem.spec.js
@@ -2,7 +2,7 @@ import getCatalogueItem from './getCatalogItem';
 import mockAxios from 'jest-mock-axios';
 import { mockCatalogueItemResponse } from '../../../__mocks__/mockCatalogueItemResponse';
 
-describe('getCatalogueItem',  () => {
+describe('getCatalogueItem', () => {
     it('should return expected values', async () => {
         mockAxios.get.mockResolvedValueOnce({ data: mockCatalogueItemResponse });
         const response = await getCatalogueItem(1);
