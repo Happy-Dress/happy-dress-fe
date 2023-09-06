9e2a496e85ac441049a228981b8b1c2279488139
Koderkup <koderkup@gmail.com>
2023-09-06 08:54:03 +0300

    feat(HAP-286): listing right and left

--- src/common/ui/components/Image/EnchancedImage/EnhancedImage.jsx
+++ src/common/ui/components/Image/EnchancedImage/EnhancedImage.jsx
@@ -5,11 +5,43 @@ import CachedImage from '../CachedImage';
 import PropTypes from 'prop-types';
 import ErrorLoadingImage from '../ErrorLoadingImage';
 import ZoomableImage from '../ZoomableImage';
+import { useSelector } from 'react-redux';
 
 const ERROR_LABEL = 'Прозошла ошибка при загрузке фотографии';
-const EnhancedImage = ({ imageUrl, alt, shouldDisplayTextError, isZoomable, widthSkeleton, heightSkeleton }) => {
+const EnhancedImage = ({
+    imageUrl,
+    alt,
+    shouldDisplayTextError,
+    isZoomable,
+    widthSkeleton,
+    heightSkeleton,
+}) => {
     const [isLoading, setIsLoading] = useState(true);
     const [isErrorLoading, setIsErrorLoading] = useState(false);
+    const [currentImageUrl, setCurrentImageUrl] = useState(imageUrl);
+    const productColorImagesURLs = useSelector(
+        (state) => state.product.productColorImages.imageURLs
+    );
+
+    const handleRightClick = () => {
+        let index = productColorImagesURLs.indexOf(currentImageUrl);
+        const limit = productColorImagesURLs.length - 1;
+        if (index <= limit) {
+            index !== limit ? (index += 1) : index;
+            setCurrentImageUrl(productColorImagesURLs[index]);
+        }
+    };
+
+    const handleLeftClick = () => {
+        let index = productColorImagesURLs.indexOf(currentImageUrl);
+        const limit = 0;
+        if (index < limit) {
+            index = limit;
+            return false;
+        }
+        index = index !== limit ? (index -= 1) : index;
+        setCurrentImageUrl(productColorImagesURLs[index]);
+    };
 
     const handleError = () => {
         setIsErrorLoading(true);
@@ -25,24 +57,37 @@ const EnhancedImage = ({ imageUrl, alt, shouldDisplayTextError, isZoomable, widt
 
     return (
         <div className={s.ProductImage}>
-            <LoadingSkeleton isLoading={isLoading} imageUrl={imageUrl} width={widthSkeleton} height={heightSkeleton}/>
+            <LoadingSkeleton
+                isLoading={isLoading}
+                imageUrl={imageUrl}
+                width={widthSkeleton}
+                height={heightSkeleton}
+            />
             <ErrorLoadingImage
                 isError={isErrorLoading}
                 shouldDisplayRetryButton={true}
                 onClick={handleClickOnError}
                 label={shouldDisplayTextError && ERROR_LABEL}
             />
-            {!isErrorLoading && <div className={s.ProductImage_hidden} style={{ display: isLoading ? 'none' : 'block' }}>
-                <ZoomableImage prohibitZoom={!isZoomable}>
-                    <CachedImage
-                        onLoad={() => setIsLoading(false)}
-                        onError={handleError}
-                        src={imageUrl}
-                        alt={alt}
-                    />
-                </ZoomableImage>
-            </div>
-            }
+            {!isErrorLoading && (
+                <div
+                    className={s.ProductImage_hidden}
+                    style={{ display: isLoading ? 'none' : 'block' }}
+                >
+                    <ZoomableImage
+                        prohibitZoom={!isZoomable}
+                        handleLeftClick={handleLeftClick}
+                        handleRightClick={handleRightClick}
+                    >
+                        <CachedImage
+                            onLoad={() => setIsLoading(false)}
+                            onError={handleError}
+                            src={currentImageUrl}
+                            alt={alt}
+                        />
+                    </ZoomableImage>
+                </div>
+            )}
         </div>
     );
 };
--- src/common/ui/components/Image/ZoomableImage/ZoomableImage.jsx
+++ src/common/ui/components/Image/ZoomableImage/ZoomableImage.jsx
@@ -1,9 +1,16 @@
 import React, { useState } from 'react';
 import s from './ZoomableImage.module.scss';
 import { ReactComponent as CloseIcon } from '../../../../../assets/images/closeIcon.svg';
+import { ReactComponent as SwipeLeft } from '../../../../../assets/images/swipeLeft.svg';
+import { ReactComponent as SwipeRight } from '../../../../../assets/images/swipeRight.svg';
 import PropTypes from 'prop-types';
 
-const ZoomableImage = ({ prohibitZoom, children }) => {
+const ZoomableImage = ({
+    prohibitZoom,
+    children,
+    handleLeftClick,
+    handleRightClick
+}) => {
     const [isExpanded, setIsExpanded] = useState(false);
 
     const toggleExpand = () => {
@@ -11,17 +18,26 @@ const ZoomableImage = ({ prohibitZoom, children }) => {
     };
 
     return (
-        <div className={isExpanded ? s.ZoomableImage_expanded : s.ZoomableImage} onClick={prohibitZoom ? undefined : toggleExpand}>
+        <div className={isExpanded ? s.ZoomableImage_expanded : s.ZoomableImage}>
             {isExpanded && (
                 <div className={s.ZoomableImage_overlay}>
                     <div className={s.ZoomableImage_closeIcon} onClick={toggleExpand}>
-                        <CloseIcon/>
+                        <CloseIcon />
+                    </div>
+                    <div className={s.ZoomableImage_swipeLeft}>
+                        <SwipeLeft onClick={handleLeftClick} />
                     </div>
                     {children}
+                    <div className={s.ZoomableImage_swipeRight}>
+                        <SwipeRight onClick={handleRightClick} />
+                    </div>
                 </div>
             )}
             {!isExpanded && (
-                <div className={s.ZoomableImage_overlay} onClick={prohibitZoom ? undefined : toggleExpand}>
+                <div
+                    className={s.ZoomableImage_overlay}
+                    onClick={prohibitZoom ? undefined : toggleExpand}
+                >
                     {children}
                 </div>
             )}
@@ -33,8 +49,10 @@ ZoomableImage.propTypes = {
     prohibitZoom: PropTypes.bool,
     children: PropTypes.oneOfType([
         PropTypes.arrayOf(PropTypes.node),
-        PropTypes.node
+        PropTypes.node,
     ]).isRequired,
+    handleLeftClick: PropTypes.func,
+    handleRightClick: PropTypes.func,
 };
 
 export default ZoomableImage;
